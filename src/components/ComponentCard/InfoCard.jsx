e15906b57330020bf1ab6dbb2c9af0784f629977
Neeraj Butola <Neerajbutola234@gmail.com>
2024-05-02 21:14:23 +0530

    ModeratorInfoCard

--- src/components/ComponentCard/InfoCard.jsx
+++ src/components/ComponentCard/InfoCard.jsx
@@ -7,7 +7,7 @@ import Button from "@mui/material/Button";
 import Chip from "@mui/material/Chip";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
 import GroupIcon from "@mui/icons-material/Group";
-import StarIcon from "@mui/icons-material/Star";
+import LocalFireDepartmentIcon from "@mui/icons-material/LocalFireDepartment";
 import MoneyIcon from "@mui/icons-material/CurrencyRupee";
 import CodeIcon from "@mui/icons-material/Code";
 
@@ -40,24 +40,55 @@ export default function InfoCard({
     <div
       style={{
         display: "flex",
+        padding: "5px 5px",
+
         alignItems: "center",
         justifyContent: "center",
       }}
     >
-      <Card sx={{ maxWidth: 786 }}>
+      <Card
+        style={{
+          marginTop: "10px",
+          marginBottom: "10px",
+
+          borderRadius: "8px",
+          borderColor: "gray", // Set border color to gray
+          borderWidth: "2px", // Optionally set border width
+          borderStyle: "solid", // Optionally set border style
+          boxShadow: "0px 0px 10px #888888", // Shadow border
+          backgroundColor: "rgba(255, 255, 255, 0.8)", // Set background color to white with 80% opacity
+        }}
+        sx={{ maxWidth: 786 }}
+      >
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
             {title}
           </Typography>
 
-          <div style={style}>
-            <MoneyIcon />
-            <span style={{ fontWeight: 700 }}>{money}</span>
+          <div style={{ marginTop: "5px" }}>
+            {/* <Chip label="Small" label="Min" size="small" />
+            <span>points</span> */}
+            {/* <Chip label="Small" label="Min" size="small" />
+            <span style={{ fontWeight: 700, fontSize: "14px" }}>
+              {money}
+            </span>{" "} */}
+            <div
+              style={{
+                display: "flex",
+                gap: "10px",
+              }}
+            >
+              Min
+              <LocalFireDepartmentIcon />
+              {points}
+            </div>
           </div>
 
-          <Typography variant="body2" color="text.secondary">
-            {displayContent}
-          </Typography>
+          <div style={{ marginTop: "8px" }}>
+            <Typography variant="body2" color="text.secondary">
+              {displayContent}
+            </Typography>
+          </div>
           {showMore && (
             <CardActions>
               {languages.map((language) => (
@@ -79,14 +110,45 @@ export default function InfoCard({
               marginTop: "8px",
             }}
           >
-            <Chip
-              icon={<AccessTimeIcon />}
-              label={`${days} days`}
-              variant="outlined"
-            />
-            <Chip icon={<GroupIcon />} label={`${people}`} variant="outlined" />
-            <Chip icon={<StarIcon />} label={`${points}`} variant="outlined" />
-            <Chip icon={<MoneyIcon />} label={`${points}`} variant="outlined" />
+            <div
+              style={{
+                display: "flex",
+                gap: "10px",
+              }}
+            >
+              <AccessTimeIcon />
+              {days}
+            </div>
+            <div
+              style={{
+                display: "flex",
+                gap: "10px",
+              }}
+            >
+              <GroupIcon />
+              {people}
+            </div>
+
+            <div
+              style={{
+                display: "flex",
+                gap: "10px",
+              }}
+            >
+              <LocalFireDepartmentIcon />
+              {points}
+            </div>
+
+            <div
+              style={{
+                display: "flex",
+                gap: "10px",
+              }}
+            >
+              <MoneyIcon />
+              {points}
+            </div>
+
             <Button size="small" onClick={() => setShowMore(!showMore)}>
               {showMore ? "See Less" : "See More"}
             </Button>
---
+++ src/components/ModeratorComponent/ModeratorInfoCard.jsx
@@ -0,0 +1,118 @@
+import { useState } from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Link from "@mui/material/Link";
+import Chip from "@mui/material/Chip";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import GroupIcon from "@mui/icons-material/Group";
+import StarIcon from "@mui/icons-material/Star";
+import MoneyIcon from "@mui/icons-material/CurrencyRupee";
+import CodeIcon from "@mui/icons-material/Code";
+
+export default function ModeratorInfoCard({
+  content,
+  title,
+  aboutMe,
+  gdriveLink,
+  pastProjectsAndCredentials,
+}) {
+  const [showMore, setShowMore] = useState(false);
+
+  const paragraphs = content.split("\n");
+  const displayContent = showMore ? content : paragraphs.slice(0, 2).join("\n");
+
+  const style = {
+    display: "inline-block",
+    padding: "5px 5px",
+    fontSize: "16px",
+    lineHeight: "20px",
+    borderRadius: "25px",
+    backgroundColor: "#808080", // Gray color
+    color: "#333",
+    boxShadow: "0px 0px 10px #888888", // Shadow border
+  };
+
+  return (
+    <div
+      style={{
+        marginTop: "5px",
+        marginBottom: "5px",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Card
+        style={{
+          marginTop: "10px",
+          marginBottom: "10px",
+
+          borderRadius: "8px",
+          padding: "2px 4px",
+          borderColor: "gray", // Set border color to gray
+          borderWidth: "2px", // Optionally set border width
+          borderStyle: "solid", // Optionally set border style
+          boxShadow: "0px 0px 10px #888888", // Shadow border
+          backgroundColor: "rgba(255, 255, 255, 0.8)", // Set background color to white with 80% opacity
+        }}
+        sx={{ maxWidth: 786 }}
+      >
+        <CardContent>
+          <Typography
+            style={{ fontWeight: "700", fontSize: "28px" }}
+            gutterBottom
+            variant="h5"
+            component="div"
+          >
+            {title}
+          </Typography>
+          <Typography gutterBottom variant="h5" component="div">
+            About
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {displayContent}
+          </Typography>
+        </CardContent>
+
+        {/* <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Why CHoose me for the project
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {displayContent}
+          </Typography>
+        </CardContent> */}
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Past Projects, Portfolio, Credentials
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {pastProjectsAndCredentials}
+          </Typography>
+        </CardContent>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Google Drive Link
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            <Link href={gdriveLink} target="_blank" rel="noopener noreferrer">
+              {gdriveLink}
+            </Link>
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button variant="contained" color="primary" style={{ flex: 1 }}>
+            Accept
+          </Button>
+
+          <Button variant="contained" color="error" style={{ flex: 1 }}>
+            Reject
+          </Button>
+        </CardActions>
+      </Card>
+    </div>
+  );
+}
